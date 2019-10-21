33df7ac31268f184e3db8a2a7c61ebeb7e51f906
Leonardo Santiago <leonardo.s13@hotmail.com>
2019-10-21 19:42:33 -0300

    Club Athletico Paranaense - Ecommerce

---
+++ Athletico/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Athletico.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        public Produto() { CriadoEm = DateTime.Now; }
+        [Key]
+
+        public int ProdutoId { get; set; }
+
+        public
+    }
+}
